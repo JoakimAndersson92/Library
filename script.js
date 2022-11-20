b3f3286a8c722435f78bb3874e74ad12a32b7a95
Joakim <joakim.andersson920202@gmail.com>
2022-11-20 15:44:44 +0100

    Add index, style and script

---
+++ script.js
@@ -0,0 +1,13 @@
+const library = [];
+
+function Books(title, author, pages){
+    this.title = title
+    this.author = author
+    this.pages = pages
+}
+
+Books.prototype.isRead = function () {
+    //if the book is read, fill radio box
+    //and return answer
+}
+
