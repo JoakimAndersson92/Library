bb967104f46cedfadcf636310cdb85be7fe0fd12
Joakim <joakim.andersson920202@gmail.com>
2022-11-22 13:23:20 +0100

    wildcard

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ var authorInput = document.querySelector('#author');
 var pagesInput = document.querySelector('#pages');
 
 const submitButton = document.querySelector('.button');
+const bookCardHolder = document.querySelector('.book-cards');
+const addBookButton = document.querySelector('#add-book')
+const formWrapper = document.querySelector('#form-wrapper');
+const form = document.querySelector('form');
+const container =  document.querySelector('.container');
 
 console.log(authorInput.value);
 
@@ -13,6 +18,8 @@ console.log(authorInput.value);
 
 submitButton.addEventListener('click', (event) => { event.preventDefault() });
 submitButton.addEventListener('click', addBookToLib);
+submitButton.addEventListener('click', createBookCard);
+addBookButton.addEventListener('click', openFormWindow);
 
 
 
@@ -22,15 +29,54 @@ function Book(title, author, pages) {
     this.pages = pages
 }
 
-function addBookToLib() {
+Book.prototype.isRead = function(){
+    console.log("read");
+}
 
+
+
+
+
+function addBookToLib() {
     let newBook = new Book(titleInput.value, authorInput.value, pagesInput.value);
     library[counter] = newBook;
+}
 
-    library.forEach(book => console.log(book));
+function openFormWindow(){
+    formWrapper.style.display = "block";
+    form.style.display = "grid";
+
+}
+
+function createBookCard(){
+    let createButton = document.createElement('button');
+    createButton.id = "exit-button";
+    let createNewDiv = document.createElement('div');
+
+    createNewDiv.classList.add("card");
+    createNewDiv.setAttribute("data-index", counter);
+    bookCardHolder.appendChild(createNewDiv);
+
+    let pointToNewCard = document.querySelector('.book-cards').lastChild;
+    pointToNewCard.appendChild(createButton);
+
+
+    for (let i = 0; i < 3; i++) {
+        let createParaG = document.createElement('p');
+        pointToNewCard.appendChild(createParaG);
+    }
+    console.log(library);
+    let firstPara = pointToNewCard.childNodes[1];
+    let secondPara = pointToNewCard.childNodes[2];
+    let thirdPara = pointToNewCard.childNodes[3];
+    firstPara.textContent = library[counter].title;
+    secondPara.textContent = library[counter].author;
+    thirdPara.textContent = library[counter].pages;
+
+    formWrapper.style.display = "none";
 
     counter++;
-
+    
 }
 
 
