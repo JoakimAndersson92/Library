b1eb34d639bbf681a3f28dbf3f151f62a1f09c17
Joakim <joakim.andersson920202@gmail.com>
2022-11-23 09:45:04 +0100

    wildcard

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const library = [];
-var counter = 0;
 
 var titleInput = document.querySelector('#title');
 var authorInput = document.querySelector('#author');
 var pagesInput = document.querySelector('#pages');
 
 const submitButton = document.querySelector('.button');
+const haveReadCheckbox = document.querySelector('[type="checkbox"]');
 const bookCardHolder = document.querySelector('.book-cards');
 const addBookButton = document.querySelector('#add-book')
 const formWrapper = document.querySelector('#form-wrapper');
@@ -16,30 +16,23 @@ console.log(authorInput.value);
 
 
 
-submitButton.addEventListener('click', (event) => { event.preventDefault() });
+submitButton.addEventListener('click', (event) => {event.preventDefault()});
 submitButton.addEventListener('click', addBookToLib);
 submitButton.addEventListener('click', createBookCard);
 addBookButton.addEventListener('click', openFormWindow);
 
 
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
+    this.read = read
 }
-
-Book.prototype.isRead = function(){
-    console.log("read");
-}
-
-
-
-
 
 function addBookToLib() {
     let newBook = new Book(titleInput.value, authorInput.value, pagesInput.value);
-    library[counter] = newBook;
+    library.push(newBook);
 }
 
 function openFormWindow(){
@@ -48,35 +41,55 @@ function openFormWindow(){
 
 }
 
+
 function createBookCard(){
-    let createButton = document.createElement('button');
-    createButton.id = "exit-button";
-    let createNewDiv = document.createElement('div');
+    let createDiv = document.createElement('div');
+    let createPara = document.createElement('p');
+    let createExit = document.createElement('button');
 
-    createNewDiv.classList.add("card");
-    createNewDiv.setAttribute("data-index", counter);
-    bookCardHolder.appendChild(createNewDiv);
-
-    let pointToNewCard = document.querySelector('.book-cards').lastChild;
-    pointToNewCard.appendChild(createButton);
-
-
-    for (let i = 0; i < 3; i++) {
-        let createParaG = document.createElement('p');
-        pointToNewCard.appendChild(createParaG);
-    }
-    console.log(library);
-    let firstPara = pointToNewCard.childNodes[1];
-    let secondPara = pointToNewCard.childNodes[2];
-    let thirdPara = pointToNewCard.childNodes[3];
-    firstPara.textContent = library[counter].title;
-    secondPara.textContent = library[counter].author;
-    thirdPara.textContent = library[counter].pages;
-
-    formWrapper.style.display = "none";
-
-    counter++;
+    createDiv.classList.add('card');
+    createExit.classList.add('exit-button');
+    createExit.setAttribute("data-index", library.length);
+    createDiv.setAttribute("data-index", library.length);
     
+
+    bookCardHolder.appendChild(createDiv);
+    bookCardHolder.lastChild.appendChild(createExit);
+    let newCardDiv = bookCardHolder.lastChild;
+
+    for (let i = 0; i < 4; i++) {
+        let createPara = document.createElement('p');
+        newCardDiv.appendChild(createPara);
+    }
+    
+
+    let exit = document.querySelector('.book-cards').lastChild.children[0];
+    let para1 = document.querySelector('.book-cards').lastChild.children[1];
+    let para2 = document.querySelector('.book-cards').lastChild.children[2];
+    let para3 = document.querySelector('.book-cards').lastChild.children[3];
+    let para4 = document.querySelector('.book-cards').lastChild.children[4];
+
+    exit.textContent = "X";
+    para1.textContent = library[library.length -1].title;
+    para2.textContent = library[library.length -1].author;
+    para3.textContent = library[library.length -1].pages;
+
+    if(haveReadCheckbox.checked){
+    para4.textContent = "Complete";
+    }
+    else{
+        para4.textContent = "Not read";          
+    }
+
+    exit.addEventListener('click', removeCard);
+  
+    formWrapper.style.display = "none";
+    form.reset();
+}
+
+function removeCard(){
+    
+    this.parentElement.style.display = "none";
 }
 
 
